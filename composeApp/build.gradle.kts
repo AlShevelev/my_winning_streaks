94e91eab8408dffed1503b5d929ec1ae4e4a28e7
Alexander Shevelev <al.e.shevelev@gmail.com>
2025-11-27 23:50:23 +0300

    Background image has been added

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -39,8 +39,11 @@ kotlin {
             implementation(compose.ui)
             implementation(compose.components.resources)
             implementation(compose.components.uiToolingPreview)
+
             implementation(libs.androidx.lifecycle.viewmodelCompose)
             implementation(libs.androidx.lifecycle.runtimeCompose)
+
+            implementation(libs.components.resources)
 
             implementation(libs.koin.core)
 
